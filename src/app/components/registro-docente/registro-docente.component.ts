3be502782e7f40e0a71856402c5fbf105c806044
EduArdo11x-X <kevin.cajeca.est@tecazuay.edu.ec>
2024-07-18 10:07:43 -0500

    INGRESANDO NUEVOS CAMBIOS EN REGISTRO DOCENTE. COMO EL BOTON Y LA NAVEGACION

---
+++ src/app/components/registro-docente/registro-docente.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'; // Asegúrate de importar NgForm aquí
+
+
+@Component({
+  selector: 'app-registro-docente',
+  templateUrl: './registro-docente.component.html',
+  styleUrls: ['./registro-docente.component.css']
+})
+export class RegistroDocenteComponent {
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      // Aquí podrías enviar los datos del formulario al servidor o hacer alguna otra acción
+      console.log(form.value);
+    }
+  }
+
+}
